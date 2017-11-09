655c5262a4757d032f56c39be03561c4724702c1
hillr <hillr@LR064W92-1718.ellis.int>
2017-11-09 12:31:37 -0500

    We're going to add to it later.

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,66 @@
 
 public class Card {
 
+	//attributes
+	private char suit;
+	private char rank;
+	
+	private static char [] SUITS = {'S', 'D', 'H', 'C'};
+	private static char [] RANKS = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 
+			'J', 'Q', 'K'};
+	
+	
+	//methods 
+	// ---- overload the constructor (make more than one)
+	//random card (no parameters)
+	public Card () {
+		int num = (int)(Math.random() * SUITS.length);
+		//random number = spot in the list
+		//save card suit as random suit
+		suit = SUITS[num];
+		
+		num = (int)(Math.random() * RANKS.length);
+		rank = RANKS[num];
+		
+	}
+	
+	//constructor that assigns specific values
+	public Card(char s, char r) {
+		suit = s;
+		setRank(r);
+		
+	}
+	
+	//set the rank within our requirements (parameters)
+	public void setRank(char r) {
+		//check to see if it's in RANKS
+		for (int i = 0; i < RANKS.length; i++) {
+			if (r == RANKS [i]) {
+				rank = r;
+				return;
+			}
+		}
+		System.out.println("Rank not found in list.");
+		
+		
+	}
+	
+	//set suit within our requirements
+	//set the rank within our requirements (parameters)
+		public void setSuit(char s) {
+			//check to see if it's in RANKS
+			for (int i = 0; i < SUITS.length; i++) {
+				if (s == SUITS [i]) {
+					suit = s;
+					return;
+				}
+			}
+			System.out.println("Suit not found in list.");
+	
+		}
+	//toString method
+	public String toString() {
+		String cardInfo = "Rank: " + rank + " Suit: " + suit;
+		return cardInfo;
+	}
 }
--- src/WarGame.java
+++ src/WarGame.java
@@ -2,7 +2,15 @@
 public class WarGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Card c1 = new Card();
+		System.out.println(c1);
+		
+		char suit = 'D';
+		char rank = '2';
+		
+		Card c2 = new Card(suit, rank);
+		System.out.println(c2);
+		
 
 	}
 
